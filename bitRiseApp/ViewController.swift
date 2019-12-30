112680b4c43cbdd1e00fc219387433e3c49e78ac
Alessio Saggiomo <alessio.saggiomo@wisemotions.com>
2019-12-30 13:24:30 +0100

    first commit

---
+++ bitRiseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitRiseApp
+//
+//  Created by Alessio Saggiomo on 30/12/2019.
+//  Copyright © 2019 Alessio Saggiomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
